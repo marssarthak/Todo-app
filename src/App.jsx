70a73db6b5d605fd4ff68ec55f540673d8491721
Sarthak Vaish <sarthakvaish184@gmail.com>
2022-03-27 10:52:06 +0530

    Nothign special

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 import ToDo from "./ThingsToDo"
 function App() {
   return (
-  <div className="">
+  <div>
     <Header/>
     <div className="px-4 sm:px-40 py-10">
       <h1 className="font-bold text-3xl text-gray-900">Things to get done</h1>
